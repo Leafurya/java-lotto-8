f4bf950491319726a0a2399a4155ba48b5e98c7b
Leafurya <audrb5300@naver.com>
2025-11-01 15:25:40 +0900

    feat: 사용자 입력 받기 구현
    구입 금액 입력, 당첨 번호 입력, 보너스 번호 입력 받기 구현

---
+++ src/main/java/lotto/InputManager.java
@@ -0,0 +1,55 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+
+public class InputManager {
+    private int parseInt(String text) {
+        int number;
+        try {
+            number = Integer.parseInt(text);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[Error] 숫자로 변환할 수 없습니다. 숫자만 입력해 주세요.");
+        }
+        return number;
+    }
+
+    public int inspectPrice(String text) {
+        int price = parseInt(text);
+        if (price % 1000 != 0) {
+            throw new IllegalArgumentException("[Error] 1000원 단위로 입력해 주세요.");
+        }
+        return price;
+    }
+
+    public int getPrice() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String text = Console.readLine();
+        return inspectPrice(text);
+    }
+
+    public ArrayList<Integer> inspectWinningNumbers(String text) {
+        String[] parts = text.split(",");
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        for (String part : parts) {
+            numbers.add(parseInt(part));
+        }
+        return numbers;
+    }
+
+    public ArrayList<Integer> getWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String text = Console.readLine();
+        return inspectWinningNumbers(text);
+    }
+
+    public int inspectBonusNumber(String text) {
+        return parseInt(text);
+    }
+
+    public int getBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String text = Console.readLine();
+        return inspectBonusNumber(text);
+    }
+}
